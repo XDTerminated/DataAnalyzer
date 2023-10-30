7b93a1a0d9126b6cadd02e894c5268702fcb6c23
Sayam Gupta <gupta.sayam2006@gmail.com>
2023-10-29 20:08:31 -0500

    Implemented data analyzer

--- static/script.js
+++ static/script.js
@@ -1,14 +1,15 @@
-let selectedFiles;
+let selectedFiles = [];
 let formData = new FormData();
 
 document.getElementById("browse").addEventListener("click", function () {
     var fileInput = document.createElement("input");
     fileInput.type = "file";
-    fileInput.accept = ".pdf, .doc, .docx, .txt, .html, .js, .css";
+    fileInput.accept = ".pdf, .docx, .txt, .pptx, .csv";
     fileInput.multiple = true;
 
     fileInput.addEventListener("change", function () {
         selectedFiles = fileInput.files;
+        console.log(selectedFiles);
         if (selectedFiles.length > 0) {
             var filesInnerHTML = document.getElementById("files");
             var browseDiv = document.getElementById("btn");
@@ -25,7 +26,7 @@ document.getElementById("browse").addEventListener("click", function () {
             var xhr = new XMLHttpRequest();
             xhr.open("POST", "upload/");
 
-            xhr.setRequestHeader("X-CSRFToken", csrfToken); 
+            xhr.setRequestHeader("X-CSRFToken", csrfToken);
 
             xhr.send(formData);
         }
@@ -34,24 +35,40 @@ document.getElementById("browse").addEventListener("click", function () {
     fileInput.click();
 });
 
+document.getElementById("img").addEventListener("click", function () {
+    if (selectedFiles.length >= 1) {
+        console.log(selectedFiles);
 
-if (selectedFiles.length > 0) {
-    document.getElementById("browse").addEventListener("click", function () {
-        
-    });
+        if (document.getElementById("searchbar").value != "") {
+            var value = document.getElementById("searchbar").value;
+            fetch("upload/", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/x-www-form-urlencoded",
+                    "X-CSRFToken": getCookie("csrftoken"), // Include CSRF token for Django
+                },
+                body: "data=" + encodeURIComponent(value),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    // Handle the processed data and update the HTML
+                    document.getElementById("output").innerText = data.result;
+                });
+        }
+    }
+});
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+        var cookies = document.cookie.split(";");
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === name + "=") {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
 }
-
-// function getCookie(name) {
-//     var cookieValue = null;
-//     if (document.cookie && document.cookie !== "") {
-//         var cookies = document.cookie.split(";");
-//         for (var i = 0; i < cookies.length; i++) {
-//             var cookie = cookies[i].trim();
-//             if (cookie.substring(0, name.length + 1) === name + "=") {
-//                 cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-//                 break;
-//             }
-//         }
-//     }
-//     return cookieValue;
-// }
